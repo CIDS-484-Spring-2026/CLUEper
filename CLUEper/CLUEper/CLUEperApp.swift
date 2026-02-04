81d7de76c550180a2883837b422c160dd32c34c0
Elijah Belz <144381253+Elijah-02@users.noreply.github.com>
2026-02-03 20:00:59 -0600

    Starter Project - Xcode
    This is the defualt starter code provided when you create a new project with Xcode. Note to self you are using an older version as the most recent updated version of Xcode is not compatible with the macOS being run on the laptop.

---
+++ CLUEper/CLUEper/CLUEperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLUEperApp.swift
+//  CLUEper
+//
+//  Created by Elijah William Belz on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CLUEperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CLUEper/CLUEper/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CLUEper
+//
+//  Created by Elijah William Belz on 2/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
